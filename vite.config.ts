7f49ed48a4b4f37056943cfa303f78e2fa83171a
kofimuad <iamkofiagyei@gmail.com>
2026-03-12 13:58:14 +0000

    added the actual backend url target

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://tala-api-oigg.onrender.com',
+      "/api": {
+        target: " https://dev-tala-api.onrender.com",
         changeOrigin: true,
         secure: true,
       },
     },
   },
-})
+});
